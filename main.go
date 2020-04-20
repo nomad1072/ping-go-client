3ca7c76e4425a614396ba3bccc355807647b990e
nomad1072 <siddharthlanka@gmail.com>
2020-04-19 22:42:25 -0600

    :bug: fix bug in count var and update tests

--- main.go
+++ main.go
@@ -3,15 +3,31 @@ package main
 import (
 	"flag"
 	"fmt"
+	"math"
+	"net"
 
 	"github.com/nomad1072/ping-go-client/ping"
 )
 
+func validateArgs(count int, ip string) (string, bool) {
+	ipaddress, _ := net.ResolveIPAddr("ip", ip)
+	if ipaddress == nil {
+		return "invalid IP address/hostname provided", false
+	}
+
+	if count < 0 {
+		return "count accepts positive integers", false
+	}
+
+	return "All arguments valid", true
+}
+
 func fetchArgs() (string, int, int, string) {
 	var ip = flag.String("hostname", "cloudflare.com", "The ip or the hostname")
-	var count = flag.Int("count", 20, "Number of requests to be made")
+	var count = flag.Int("count", math.MaxInt64, "Number of requests to be made")
 	var ttl = flag.Int("ttl", 255, "Number of maximum network hops")
 	var sourceIP = flag.String("sourceip", "2601:281:8380:3570:f4db:2ca7:3f8f:a109", "Specify source IP for IPv6")
+
 	flag.Parse()
 	// var address = "2001:4860:4860::8888"
 	fmt.Println("Ip: ", *ip)
@@ -21,5 +37,9 @@ func fetchArgs() (string, int, int, string) {
 
 func main() {
 	ip, count, ttl, sourceIpv6 := fetchArgs()
-	ping.Ping(ip, count, ttl, sourceIpv6)
+	message, ok := validateArgs(count, ip)
+	println("Message: ", message)
+	if ok {
+		ping.Ping(ip, count, ttl, sourceIpv6)
+	}
 }
--- main_test.go
+++ main_test.go
@@ -11,11 +11,29 @@ func TestArgs(t *testing.T) {
 	os.Args = append(os.Args, "-hostname=amazon.com")
 	os.Args = append(os.Args, "-count=50")
 
-	actualHostname, actualCount, _, _ := fetchArgs()
-	if actualHostname != expectedHostname {
-		t.Errorf("Test failed, expected: '%s', got: '%s'", expectedHostname, actualHostname)
-	}
-	if expectedCount != actualCount {
-		t.Errorf("Test failed, expected: '%d', got: '%d'", expectedCount, actualCount)
-	}
+	t.Run("testCommandLineArgs", func(t *testing.T) {
+		actualHostname, actualCount, _, _ := fetchArgs()
+		if actualHostname != expectedHostname {
+			t.Errorf("Test failed, expected: '%s', got: '%s'", expectedHostname, actualHostname)
+		}
+		if expectedCount != actualCount {
+			t.Errorf("Test failed, expected: '%d', got: '%d'", expectedCount, actualCount)
+		}
+	})
+
+	t.Run("validateCommandLineArgs", func(t *testing.T) {
+		message, _ := validateArgs(-1, "googkle.com")
+		wrongHostName := "invalid IP address/hostname provided"
+		if message != wrongHostName {
+			t.Errorf("Test failed, provided incorrect hostname but received ambiguous output")
+		}
+	})
+
+	t.Run("validateCommandLineArgs", func(t *testing.T) {
+		message, _ := validateArgs(-1, "google.com")
+		wrongHostName := "count accepts positive integers"
+		if message != wrongHostName {
+			t.Errorf("Test failed, provided incorrect count value but client accepts given value")
+		}
+	})
 }
